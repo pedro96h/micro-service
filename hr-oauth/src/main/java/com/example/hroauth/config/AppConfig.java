a33ba8d227137c318529d1f264357ab3d8fea3a3
Pedro Oliveira <pedro96h@gmail.com>
2021-06-02 01:43:18 -0300

    security configs to config server

--- hr-oauth/src/main/java/com/example/hroauth/config/AppConfig.java
+++ hr-oauth/src/main/java/com/example/hroauth/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.example.hroauth.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,6 +10,9 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 public class AppConfig {
 
+	@Value("${jwt.secret}")
+	private String jwtSecret;
+	
 	@Bean
 	public BCryptPasswordEncoder CryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -17,7 +21,7 @@ public class AppConfig {
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter TokenConverter = new JwtAccessTokenConverter();
-		TokenConverter.setSigningKey("MY-SECRET-KEY");
+		TokenConverter.setSigningKey(jwtSecret);
 		return TokenConverter;
 	}
 
